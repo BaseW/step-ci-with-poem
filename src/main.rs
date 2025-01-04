418b9d4a8b6b95c59c2407c5eb43241870acc642
wada <wadayugo@keio.jp>
2025-01-04 13:33:27 +0900

    feat: add swagger ui

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
-use poem::{get, handler, listener::TcpListener, web::Path, Route, Server};
+use poem::{listener::TcpListener, Route};
+use poem_openapi::{param::Query, payload::PlainText, OpenApi, OpenApiService};
 
-#[handler]
-fn hello(Path(name): Path<String>) -> String {
-    format!("hello: {}", name)
+struct Api;
+
+#[OpenApi]
+impl Api {
+    #[oai(path = "/hello", method = "get")]
+    async fn index(&self, name: Query<Option<String>>) -> PlainText<String> {
+        match name.0 {
+            Some(name) => PlainText(format!("hello, {}!", name)),
+            None => PlainText("hello!".to_string()),
+        }
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let app = Route::new().at("/hello/:name", get(hello));
-    Server::new(TcpListener::bind("0.0.0.0:3000"))
+    let api_service =
+        OpenApiService::new(Api, "Hello World", "1.0").server("http://localhost:3000/api");
+    let ui = api_service.swagger_ui();
+    let app = Route::new().nest("/api", api_service).nest("/", ui);
+
+    poem::Server::new(TcpListener::bind("0.0.0.0:3000"))
         .run(app)
         .await
 }
